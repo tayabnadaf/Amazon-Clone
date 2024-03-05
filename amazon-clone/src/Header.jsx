2c387357a7038649549d04ff17039f7a79cf2bd8
Tayab Nadaf <tayabnadaf07@gmail.com>
2024-03-05 21:22:48 +0530

    Completed NavBar

--- amazon-clone/src/Header.jsx
+++ amazon-clone/src/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SreachIcon from "@mui/icons-material/Search";
+import ShopingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
     <nav className="header">
-      {/*logo on left*/}
       <Link to="/login">
         <img
           className="header__logo"
@@ -15,18 +15,39 @@ function Header() {
         />
       </Link>
 
-      {/*the search box*/}
       <div className="header__search">
         <input type="text" className="header__searchIntput" />
         <SreachIcon className="header__searchIcon" />
       </div>
 
-      {/*2 links*/}
       <div className="header__linkBar">
-        {/* sign link */}
-        {/* return & order link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__opLone">Hello,sign in</span>
+            <span className="header__opLtwo">Account & Lists</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__opLone">Returns</span>
+            <span className="header__opLtwo"> & Order</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__opLone">Your</span>
+            <span className="header__opLtwo">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShopingBasketIcon />
+            <span className="header__opLtwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
-      {/*cart logo with number on it*/}
     </nav>
   );
 }
